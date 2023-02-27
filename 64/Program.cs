de1aa79ee708c69d0d99bf36c0b5576c9aed40ee
Stanislav_Lushnikov <lust48h@gmail.com>
2023-02-27 23:45:35 +0300

    add homework 64

---
+++ 64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("Введите начальное число для декремента: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"N = {num} -> '{GetSequenceDecrimentNum(num)}'");
+
+string GetSequenceDecrimentNum(int baseNumber)
+{
+    if (baseNumber == 1) return baseNumber.ToString();
+    else 
+    {
+        string result = baseNumber + ", " + GetSequenceDecrimentNum(baseNumber - 1);
+        return result;
+    }
+}
