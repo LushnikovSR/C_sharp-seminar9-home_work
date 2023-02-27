058fb255b6ef2450e90d6b5c3d37dce2fba7ddfc
Stanislav_Lushnikov <lust48h@gmail.com>
2023-02-27 23:45:57 +0300

    add homework 66

---
+++ 66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите нижнее значение диапазона: ");
+int lowNum = int.Parse(Console.ReadLine());
+Console.Write("Введите верхнее значение диапазона: ");
+int highNum = int.Parse(Console.ReadLine());
+
+Console.Write($"M = {lowNum}; N = {highNum} -> {GetSumSequence(lowNum, highNum)}");
+
+int GetSumSequence(int startNum, int endNum)
+{
+    if (endNum == startNum) return startNum;
+    else 
+    {
+        int result = endNum + GetSumSequence(startNum, endNum - 1);
+        return result;
+    }
+}
+
